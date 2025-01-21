a3c51f3d43a1c3725f26ff4a082e793724711be0
Jessica <eng.diias@gmail.com>
2025-01-21 13:51:43 -0300

    Game Seed

--- src/main/java/com/Games/dslistt/entities/Game.java
+++ src/main/java/com/Games/dslistt/entities/Game.java
@@ -21,23 +21,30 @@ public class Game {
 	
 	@Column(name="game_year")
 	private Integer year;
-	private Integer genre;
-	private String plataform;
+	private String genre;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer genre, String plataform, String imgUrl, String shortDescription,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription,
 			String longDescription) {
 
 		this.id = id;
 		this.title = title;
+		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -58,21 +65,39 @@ public class Game {
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 
-	public Integer getGenre() {
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
 		return genre;
 	}
 
-	public void setGenre(Integer genre) {
+	public void setGenre(String genre) {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String plataforms) {
+		this.platforms = plataforms;
+	}
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
